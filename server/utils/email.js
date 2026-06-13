a2e2b91601b9850fe71a43be83e4ba367dc89cfd
devang <devangkumar12112005@gmail.com>
2026-06-13 22:26:06 +0530

    Add SMTP timeouts to prevent hanging

--- server/utils/email.js
+++ server/utils/email.js
@@ -19,7 +19,9 @@ const createTransporter = () => {
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
-      }
+      },
+      connectionTimeout: 5000,
+      socketTimeout: 5000
     });
   }
 
